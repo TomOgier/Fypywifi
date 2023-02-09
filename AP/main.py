0fcb47ab124a03c64208ff64644b44e0e339314d
Tom Ogier <amir.benali@gmail.com>
2023-02-09 09:01:23 +0100

    premiere modification ajout algo pspoll

--- AP/main.py
+++ AP/main.py
@@ -4,28 +4,58 @@ import pycom
 import time
 import machine
 
-def monitordata(pack):
+
+dataToSend = [1]
+
+waitForAck = False
+
+def accept_pack(pack):
+    return pack.data[10]==0xfc and pack.data[11]==0xf5 and pack.data[12] == 0xc4 and pack.data[13]==0x0d and pack.data[14]==0xef and pack.data[15]==0x64
+
+def accept_ack(pack):
+    return len(pack.data) - 28 == 2 and pack.data[24] == 0x01 
+
+def accept_pspoll(pack):
+    return len(pack.data) - 28 == 1 and pack.data[24] == 0x02
+
+def accept_data(pack):
+    return len(pack.data) - 28 > 1 and pack.data[24] == 0x03
+
+beacon = b'\x80\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xF0\x08\xD1\xCB\x59\x00\xF0\x08\xD1\xCB\x59\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x64\x00\x31\x04\x00\x04\x46\x41\x43\x54\x01\x08\x82\x84\x8B\x96\x0C\x12\x18\x24\x03\x01\x01\x05\x04\x01\x02\x00\x00'
+ackNoData = b'\x08\x00\x00\x00\xfc\xf5\xc4\x0d\xef\x64\xf0\x08\xd1\xcb\x59\x68\xf0\x08\xd1\xcb\x59\x68\x00\x00\x01'
+data = b'\x08\x00\x00\x00\xfc\xf5\xc4\x0d\xef\x64\xf0\x08\xd1\xcb\x59\x68\xf0\x08\xd1\xcb\x59\x68\x00\x00\x03\xff'
+
+def monitor(pack):
     pk = wlan.wifi_packet()
-    if pk.data[8]==0x59 and pk.data[9] == 0x68: 
-        if len(pk.data) - 28 == 1 and pk.data[24] == 0x01:
+    if accept_pack(pk): 
+        if accept_ack(pk) and waitForAck:
+            waitForAck=False
+            dataToSend.pop()
+
+        if accept_pspoll(pk):
             print("j'ai recu un pspoll")
-            
+            if len(dataToSend) == 0:
+                wlan.send_raw(Buffer=ackNoData)
+                
+                print('acknodata envoye')
+            else:
+                #print('jai envoyé des data')
+                waitForAck = True
+                wlan.send_raw(Buffer=data)
+
+       
         
 
-pycom.heartbeat(True)
+pycom.heartbeat(False)
 wlan = WLAN()
 wlan.antenna(WLAN.EXT_ANT)
 print("Je suis l'AP")
 
 
-beacon = b'\x80\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xF0\x08\xD1\xCB\x59\x00\xF0\x08\xD1\xCB\x59\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x64\x00\x31\x04\x00\x04\x46\x41\x43\x54\x01\x08\x82\x84\x8B\x96\x0C\x12\x18\x24\x03\x01\x01\x05\x04\x01\x02\x00\x00'
-
-
-
 print(ubinascii.hexlify(machine.unique_id(),':').decode())
 #wlan.send_raw(Buffer=beacon,interface=WLAN.AP)
 
-wlan.callback(trigger=WLAN.EVENT_PKT_DATA, handler=monitordata)
+wlan.callback(trigger=WLAN.EVENT_PKT_DATA, handler=monitor)
 wlan.promiscuous(True)
 
-
+wlan.send_raw(Buffer=ackNoData)
--- STA/main.py
+++ STA/main.py
@@ -1,71 +1,51 @@
-'''
-# main.py -- put your code here!
 from network import WLAN
-import ubinascii
 import machine
+import ubinascii
 
-def pack_cb(pack):
-    mac = bytearray(6)
+listenIntervalle = 1000
+
+def accept_pack(pack):
+    return pack.data[10]==0xf0 and pack.data[11]==0x08 and pack.data[12] == 0xd1 and pack.data[13]==0xcb and pack.data[14]==0x59 and pack.data[15]==0x68
+
+def accept_ack(pack):
+    return len(pack.data) - 28 == 1 and pack.data[24] == 0x01 
+
+def accept_pspoll(pack):
+    return len(pack.data) - 28 == 1 and pack.data[24] == 0x02 
+
+def accept_data(pack):
+    return len(pack.data) - 28 >= 1 and pack.data[24] == 0x03
+
+pspoll = b'\x08\x00\x00\x00\xf0\x08\xd1\xcb\x59\x68\xfc\xf5\xc4\x0d\xef\x64\xfc\xf5\xc4\x0d\xef\x64\x00\x00\x02'
+ackData = b'\x08\x00\x00\x00\xf0\x08\xd1\xcb\x59\x68\xfc\xf5\xc4\x0d\xef\x64\xfc\xf5\xc4\x0d\xef\x64\x00\x00\x01'
+
+def monitor(pack):
     pk = wlan.wifi_packet()
-    control = pk.data[0]
-    subtype = (oxF0 & control) >> 4
-    type = 0x0C & control
-    print('camarche')
-    print(pack)
-    
+    print('a')
+    if accept_pack(pk):
+        print('a')
+        if accept_ack(pk):
+            print("j'ai recu un ack")
+            machine.sleep(listenIntervalle,True)
+            wlan.send_raw(Buffer=pspoll)
+        if accept_data(pk):
+            print("j'ai recu une data")
+            wlan.send_raw(Buffer=ackData)
+            machine.sleep(listenIntervalle,True)
+            wlan.send_raw(Buffer=pspoll)
 
-wlan = WLAN()
-#print(wlan.wifi_packet())
-wlan.init(mode=WLAN.STA,antenna=WLAN.EXT_ANT)
-
-wlan.init(power_save=True)
-nets = wlan.scan()
-for net in nets:
-    if net.ssid == "FACT":
-        print("network trouve")
-        wlan.connect(net.ssid,auth=(net.sec,"motdepasse"))
-        while not wlan.isconnected():
-            machine.idle()
-        print('WLAN est bien connecte')
-        break
-
-
-wlan.ifconfig(config=('192.168.4.10','255.255.255.0','192.168.4.1','8.8.8.8'))
-print(wlan.ifconfig())
-print(wlan.mac())
-
-
-machine.sleep(20000)
-
-wlan.callback(trigger=WLAN.EVENT_PKT_ANY,handler=pack_cb)
-wlan.promiscuous(True)
-
-pk = wlan.wifi_packet()
-print(pk)
-wlan.send_raw(Buffer=b'010110010101010101010101010101010101010101010101010101')
-
-test = b'\x00\x00\x12\x00\x2e\x48\x00\x00\x10\x02\x99\x09\xa0\x00\xb4\x00\x80\x00\xac\xb5}\x8d;0<F\xd8~\x0e\xdd\xc2U0\xde'
-test2= b'\x00\x00\x12\x00\x2e\x48\x00\x00\x10\x02\x99\x09\xa0\x00\xbb\x03\x00\x00\x40\x00\x00\x00\xff\xff\xff\xff\xff\xff\xfc\xf5\xc4\x0d\xef\x64\xff\xff\xff\xff\xff\xff\xa0\x6c\x00\x05\x46\x41\x43\x54\x4f\x01\x08\x8b\x96\x82\x84\x0c\x18\x30\x60\x32\x04\x6c\x12\x24\x48\xe6\x0e\x51\x07'
-
-wlan.send_raw(Buffer=test2)
-
-'''
-from network import WLAN
-import machine
-import ubinascii
-
-pspoll = b'\x08\x00\x00\x00\xf0\x08\xd1\xcb\x59\x68\xfc\xf5\xc4\x0d\xef\x64\xf0\x08\xd1\xcb\x59\x68\x00\x00\x01'
-       
 
 print("Je suis la STA")
-wlan = WLAN(mode=WLAN.STA, antenna=WLAN.EXT_ANT)
+wlan = WLAN(antenna=WLAN.EXT_ANT)
+
+print(ubinascii.hexlify(machine.unique_id(),':').decode())
+
+
+wlan.callback(trigger=WLAN.EVENT_PKT_DATA, handler=monitor)
+wlan.promiscuous(True)
+
 
 wlan.send_raw(Buffer=pspoll)
-
-
-wlan.callback(trigger=WLAN.EVENT_PKT_DATA, handler=pack_cb)
-print(ubinascii.hexlify(machine.unique_id(),':').decode())
-wlan.promiscuous(True)
 
 
 
